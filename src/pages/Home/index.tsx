7006c651e45839ca7e3aa001d722d3b08199f01f
marciovz <marciovz@yahoo.com.br>
2022-08-29 10:34:47 -0300

    Separado o componente intro da página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,56 +1,10 @@
-import { ShoppingCart, Timer, Package, Coffee } from 'phosphor-react'
-
-import imgIntro from '../../assets/img-intro.svg'
-
-import {
-  IntroContainer,
-  IntroContent,
-  IntroItemColumns,
-  IntroItem,
-  ProductList,
-} from './styles'
+import { Intro } from './Intro'
+import { ProductList } from './styles'
 
 export function Home() {
   return (
     <>
-      <IntroContainer>
-        <IntroContent>
-          <section>
-            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-            <h2>
-              Com o Coffee Delivery você recebe seu café onde estiver, a
-              qualquer hora
-            </h2>
-
-            <IntroItemColumns>
-              <div>
-                <IntroItem statusColor="yellowDark">
-                  <ShoppingCart size={32} weight="fill" />
-                  <p>Compra simples e segura</p>
-                </IntroItem>
-                <IntroItem statusColor="yellow">
-                  <Timer size={32} weight="fill" />
-                  <p>Entrega rápida e rastreada</p>
-                </IntroItem>
-              </div>
-              <div>
-                <IntroItem statusColor="grayDark">
-                  <Package size={32} weight="fill" />
-                  <p>Embalagem mantém o café intacto</p>
-                </IntroItem>
-                <IntroItem statusColor="purple">
-                  <Coffee size={32} weight="fill" />
-                  <p>O café chega fresquinho até você</p>
-                </IntroItem>
-              </div>
-            </IntroItemColumns>
-          </section>
-
-          <section>
-            <img src={imgIntro} alt="" />
-          </section>
-        </IntroContent>
-      </IntroContainer>
+      <Intro />
       <ProductList />
     </>
   )
