dd3b926b736c2b4bb72ab3df80b40e75c0c03408
marciovz <marciovz@yahoo.com.br>
2023-06-02 17:10:21 -0300

    Configurado o json-server, o servidor axios e adicionado as imagens dos coffees na pasta public

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,67 @@
+import { useEffect, useState } from 'react'
+
+import { api } from '../../services/api'
 import { Intro } from './Intro'
-import { ProductList } from './styles'
+import { CoffeeCard } from '../../components/CoffeeCard'
+
+import { CoffeeListContainer, CoffeeList } from './styles'
+
+interface ICoffee {
+  id: string
+  tag: string[]
+  title: string
+  description: string
+  price: number
+  image: string
+}
+
+const mockCoffeeList = [
+  {
+    id: '1',
+    tag: ['tradicional', 'teste'],
+    title: 'Expresso Tradicional',
+    description: 'O tradicional café feito com água quente e grãos moídos',
+    price: 9.9,
+    image: 'expresso_tradicional.svg',
+  },
+  {
+    id: '2',
+    tag: ['tradicional'],
+    title: 'Expresso americano',
+    description: 'Expresso diluído, menos intenso que o tradicional',
+    price: 9.9,
+    image: 'expresso_americano.svg',
+  },
+]
 
 export function Home() {
+  const [coffeeList, setCoffeeList] = useState<ICoffee[]>([])
+
+  function editShoppingCart() {}
+
+  useEffect(() => {
+    async function loadCoffees() {
+      const requestData = await api.get('/coffeeList')
+      setCoffeeList(requestData.data)
+    }
+    loadCoffees()
+  }, [])
+
   return (
     <>
       <Intro />
-      <ProductList />
+      <CoffeeListContainer>
+        <h2>Nossos cafés</h2>
+        <CoffeeList>
+          {coffeeList.map((item) => (
+            <CoffeeCard
+              key={item.id}
+              coffee={item}
+              onAddToCart={editShoppingCart}
+            />
+          ))}
+        </CoffeeList>
+      </CoffeeListContainer>
     </>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components'
 
-export const ProductList = styled.div``
+export const CoffeeListContainer = styled.div`
+  width: 70rem;
+  margin: 2rem auto 10rem;
+
+  h2 {
+    font: ${({ theme }) => theme.fonts['baloo-800']};
+    font-size: 2rem;
+    line-height: 2.3rem;
+    color: ${({ theme }) => theme.colors['base-title']};
+  }
+`
+export const CoffeeList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+  margin-top: 3.375rem;
+`
